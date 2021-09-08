66f7d2f66cf83bfd6749715f79511681f0bccae9
logoonds <jhonny_salcedo21@hotmail.com>
2021-09-08 11:04:08 -0500

    Primer punto calcular el valor total de camisas a pagar

--- index.py
+++ index.py
@@ -12,6 +12,24 @@ if (edad >=18):
 else:
     print('Usted es menor de edad!')
 
-
-    
+# =============================================================================
+# . Hacer un algoritmo que calcule el total a pagar por la compra de
+# camisas. Si se compran tres camisas o mas se aplica un descuento
+# del 30% sobre el total de la compra y si son menos de tres camisas
+# un descuento del 10%.
+# =============================================================================
+cant = int(input('Cantidad de camisas que desea comprar: '))
+val = int(input('Ingrese valor de la camisa: '))
+if (cant >= 3):
+    res = int(val * cant)
+    des = int(res * 0.3)
+    total = int(res - des)
+    print('Descuento del 30% es: ',des)
+    print('El total a pagar es: ',total)
+else:
+    res = int(val * cant)
+    des = int(res * 0.1)
+    total = int(res - des)
+    print('Descuento del 10% es: ',des)
+    print('El total a pagar es: ',total)
     
