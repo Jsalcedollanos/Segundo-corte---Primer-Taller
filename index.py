22b3db0ae35c90b3746d7fb113ab50e075fccdcb
logoonds <jhonny_salcedo21@hotmail.com>
2021-09-08 10:39:06 -0500

    Primer commit de la primera clase del segundo corte

---
+++ index.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 07:44:12 2021
+
+@author: sistema
+"""
+# primer commit en la clase 1 segundo corte.
+# Haga un algoritmo que determine que persona es mayor de edad
+edad = int(input('Ingrese su edad: '))
+if (edad >=18):
+    print('Usted es mayor de edad!')
+else:
+    print('Usted es menor de edad!')
+
+
+    
+    
