35bebf02df379622bca5ada88645efac211427a0
logoonds <jhonny_salcedo21@hotmail.com>
2021-09-08 12:00:10 -0500

    Cuarto Punto, medicion de puntos de contaminacion

--- index.py
+++ index.py
@@ -70,3 +70,26 @@ if (monto < 50000):
 if (monto >= 50000):
     cuota = (monto * 0.02)
     print('Couta a pagar del 2%: ',cuota)
+    
+# =============================================================================
+# Una fábrica ha sido sometida a un programa de control de
+# contaminación para lo cual se efectúa una revisión de los puntos de
+# contaminación generados por la fábrica. El programa de control de
+# contaminación consiste en medir los puntos que emite la fábrica en
+# cinco días de una semana y si el promedio es superior a los 170
+# puntos entonces tendrá la sanción de parar su producción por una
+# semana y una multa del 50% de las ganancias diarias cuando no se
+# detiene la producción. Si el promedio obtenido de puntos es de 170 o
+# menos entonces no tendrá ni sanción ni multa. El dueño de la fábrica
+# desea saber cuanto dinero perderá después de ser sometido a la
+# revisión.
+# =============================================================================
+medida = int(input('Ingrese la medida: en puntos de la semana: '))
+salario = int(input('Ingrese salario: '))
+promedio = (medida / 5)
+if (promedio > 170):
+    multa = salario * 0.5
+    print('Se para la producion por 1 semana!')
+    print('Total de la multa del 50% es: ',multa)
+if (promedio <= 170):  
+    print('EXCELENTE!! no tiene multa ni sancion')
